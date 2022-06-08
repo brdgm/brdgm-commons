71957a4038b441a3152ff1f606edb89bd399dbcd
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-06-08 21:28:19 +0200

    stripped down version

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Header/>
+
+  <div id="content-container" class="container-fluid mt-5 mb-5">
+    <router-view :key="$route.fullPath"/>
+  </div>
+
+  <Footer />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+import Header from '@/components/structure/Header.vue'
+import Footer from '@/components/structure/Footer.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  setup() {
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: 'global'
+    })
+
+    return { t }
+  },
+})
+</script>
