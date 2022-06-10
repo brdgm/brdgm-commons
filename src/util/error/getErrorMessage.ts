53d331da3700b9b0f4817ce6526b95253fdd5642
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-06-10 16:02:09 +0200

    simplify code

--- src/util/error/getErrorMessage.ts
+++ src/util/error/getErrorMessage.ts
@@ -2,14 +2,14 @@ import TranslatableError from "./TranslatableError"
 
 /**
  * Get error message for error
- * @param error Error object
+ * @param err Error object
  * @param translateError Translate error message
  * @returns Error message
  */
 export default function(err : unknown, translateError: (error : TranslatableError) => string) : string {
   let message
   if (err instanceof TranslatableError) {
-    message = translateError(err as TranslatableError)
+    message = translateError(err)
   }
   else if (err instanceof Error) {
     message = err.message
