7cfc3d38796bf38c8ea748041dedf53841d84942
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-05-09 12:33:31 +0200

    util/random/rollDiceDifferentValue and randomEnumDifferentValue: Allow to provide multiple current values

--- src/util/random/rollDiceDifferentValue.ts
+++ src/util/random/rollDiceDifferentValue.ts
@@ -3,18 +3,18 @@ import rollDice from './rollDice'
 /**
  * Rolls a dice, and ensures to return a different value than the given current value.
  * @param maxValue Max. dice value
- * @param currentValue Current value
+ * @param currentValue Current value(s)
  * @returns Dice value
  */
-export default function rollDiceDifferentValue(maxValue: number, currentValue: number) : number {  
-  if (maxValue < 2) {
-    throw new Error(`Unable to roll a dice ${maxValue} with a different value`)
+export default function rollDiceDifferentValue(maxValue: number, ...currentValue: number[]) : number {  
+  if (maxValue < 2 || currentValue.length > maxValue - 1) {
+    throw new Error(`Unable to roll a dice ${maxValue} with a different value.`)
   }
   const newNumber = rollDice(maxValue)
-  if (newNumber != currentValue) {
+  if (!currentValue.includes(newNumber)) {
     return newNumber
   }
   else {
-    return rollDiceDifferentValue(maxValue, currentValue)
+    return rollDiceDifferentValue(maxValue, ...currentValue)
   }
 }
