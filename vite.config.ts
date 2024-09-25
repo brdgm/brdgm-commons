c6ad0dddfbb9d1d6f3e01fbd6bce3ca41af9fcd7
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-09-25 20:00:37 +0200

    Improve support for older browsers/devices (#13)
    Apply @vitejs/plugin-legacy to use polyfills with older and modern browsers

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
     VueI18nPlugin({
       include: [path.resolve(__dirname, './src/locales/**')],
       strictMessage: false
+    }),
+    // support older browsers
+    legacy({
+      modernTargets: 'iOS >= 15, Safari >= 15',
+      modernPolyfills: true
     })
   ],
   resolve: {
