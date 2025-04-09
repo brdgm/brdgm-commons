1e44e61c6f788fb9610996018be9235c0bbfaa43
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-04-09 20:33:46 +0200

    Add util/form/toNumber

---
+++ src/util/form/toNumber.ts
@@ -0,0 +1,14 @@
+/**
+ * Converts a value from a form field to a number.
+ * Only a real number is accepted, everything else results 0.
+ * @param value Value to convert
+ * @return Number value
+ */
+export default function (value? : unknown) : number {
+  if (typeof value == 'number') {
+    return value
+  }
+  else {
+    return 0
+  }
+}
