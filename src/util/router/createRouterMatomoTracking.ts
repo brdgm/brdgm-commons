ed8c056bfa3eab7e354e728574b1d6cf5d917ac4
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-08-13 09:36:13 +0200

    omit semicolons

--- src/util/router/createRouterMatomoTracking.ts
+++ src/util/router/createRouterMatomoTracking.ts
@@ -1,7 +1,7 @@
 import { RouteRecordRaw, Router } from 'vue-router'
 import createRouter from './createRouter'
 
-declare let _paq: any;  // eslint-disable-line @typescript-eslint/no-explicit-any
+declare let _paq: any  // eslint-disable-line @typescript-eslint/no-explicit-any
 
 /**
  * Create a vue router with web hash history.
